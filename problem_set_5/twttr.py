25dd55cac9520af65cbb95a1535bde15a5923e9d
mia-sv <130096279+mia-sv@users.noreply.github.com>
2023-06-16 01:01:08 +0100

    test_twttr.py completed

---
+++ problem_set_5/twttr.py
@@ -0,0 +1,18 @@
+vowels = ["a", "e", "i", "o", "u"]
+
+def main():
+    name = input("Input: ")
+    print(f"Output: {shorten(name)}")
+
+
+def shorten(word):
+    new_word = word
+
+    for x in vowels:
+        new_word = new_word.replace(x, "").replace(x.upper(), "")
+    return new_word
+
+
+
+if __name__ == "__main__":
+    main()   
